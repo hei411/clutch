a4da7d05ea11cd8395f60a0cfa39b629a560292f
Hei Li <hei411@gmail.com>
2023-12-12 13:17:11 +0100

    ub_lift done

--- theories/ub_logic/adequacy.v
+++ theories/ub_logic/adequacy.v
@@ -229,10 +229,11 @@ Qed.
 
 
 (*epsilon limit*)
-Lemma ub_lift_epsilon_limit `{Eq: EqDecision A} `{HC:Countable A} (μ : distr A) f : (forall ε, ε>0 -> ub_lift μ f ε) -> ub_lift μ f 0.
+Lemma ub_lift_epsilon_limit `{Eq: EqDecision A} `{HC:Countable A} (μ : distr A) f ε':
+  ε'>=0 -> (forall ε, ε>ε' -> ub_lift μ f ε) -> ub_lift μ f ε'.
 Proof.
-  intros H.
-  assert (forall seq,(∀ n, 0 <= seq n <=1) -> (∀ n, ub_lift μ f (1-seq n))->ub_lift μ f (1-Sup_seq seq)) as H_limit.
+  intros Hε' H.
+  assert (forall seq,(∃ b, ∀ n, 0 <= - seq n <= b) -> (∀ n, ub_lift μ f (ε'-seq n))->ub_lift μ f (ε'-Sup_seq seq)) as H_limit.
   { clear H.
     rewrite /ub_lift.
     intros.
@@ -244,38 +245,82 @@ Proof.
     + apply upper_bound_ge_sup.
       intros.
       pose proof (H0 n P H1). rewrite rbar_le_rle. lra.
-    + apply (is_finite_bounded 0 1).
-      -- eapply (Sup_seq_minor_le _ _ 0). destruct (H 0%nat). apply H2. 
-      -- apply upper_bound_ge_sup. intros n. destruct (H n). apply H3. 
+    + destruct H as [b H].
+      apply (is_finite_bounded (-b) 0).
+      -- eapply (Sup_seq_minor_le _ _ 0). destruct (H 0%nat). apply Ropp_le_contravar in H3.
+         rewrite Ropp_involutive in H3. apply H3.
+      -- apply upper_bound_ge_sup. intros n. destruct (H n). apply Ropp_le_contravar in H2.
+         rewrite Ropp_involutive in H2. rewrite Ropp_0 in H2. done.
   }
-  replace 0%R with (1-Sup_seq (λ x,(λ n,1-1/(S n)) x)).
+  replace ε' with (ε'- Sup_seq (λ x,(λ n,-1%R/(S n)) x)).
   - apply H_limit.
-    { intros; split.
-      -- apply Rle_0_le_minus. rewrite <-Rcomplements.Rdiv_le_1.
-         ++ rewrite S_INR. rewrite <- Rcomplements.Rle_minus_l.
-            replace (_-_) with 0; try lra.
-            apply pos_INR.
-         ++ apply Rcomplements.Rlt_nat. eauto. 
-      -- assert (1/S n>=0); try lra.
-         apply Rle_ge.
+    { exists 1.
+      intros; split.
+      -- rewrite Ropp_div. rewrite Ropp_involutive.
          apply Rcomplements.Rdiv_le_0_compat; try lra.
-         apply Rcomplements.Rlt_nat. eauto.
+         rewrite S_INR. pose proof (pos_INR n).
+         lra.
+      -- rewrite Ropp_div. rewrite Ropp_involutive.
+         rewrite Rcomplements.Rle_div_l; [|apply Rlt_gt].
+         ++ assert (1<=S n); try lra.
+            rewrite S_INR.
+            pose proof (pos_INR n).
+            lra.
+         ++ rewrite S_INR.
+            pose proof (pos_INR n).
+            lra. 
     }
     intros. apply H.
-    apply Rgt_minus.
+    assert (1/(S n) > 0); try lra.
     apply Rlt_gt.
-    rewrite Rcomplements.Rlt_minus_l.
-    apply Rlt_gt.
-    apply Rminus_gt_0_lt.
-    replace (_+_-_) with (1/S n); try lra.
-    apply Rdiv_lt_0_compat; try lra.
-    rewrite S_INR. pose proof pos_INR.
-    apply Rplus_le_lt_0_compat; try lra. done.
-  - apply Rminus_diag_eq.
-    apply eq_rbar_finite.
-    admit. 
-Admitted.
-  
+    apply Rdiv_lt_0_compat; first lra.
+    rewrite S_INR. pose proof (pos_INR n); lra.
+  - assert (Sup_seq (λ x : nat, - (1)%R / S x)=0) as Hswap; last first.
+    + rewrite Hswap. erewrite<- eq_rbar_finite; [|done]. lra.
+    + apply is_sup_seq_unique. rewrite /is_sup_seq.
+      intros err.
+      split.
+      -- intros n.
+         eapply (Rbar_le_lt_trans _ (Rbar.Finite 0)); last first.
+         ++ rewrite Rplus_0_l. apply (cond_pos err).  
+         ++ apply Rge_le. rewrite Ropp_div. apply Ropp_0_le_ge_contravar.
+            apply Rcomplements.Rdiv_le_0_compat; try lra.
+            rewrite S_INR. pose proof (pos_INR n); lra.
+      -- pose (r := 1/err -1).
+         assert (exists n, r<INR n) as [n Hr]; last first.
+         ++ eexists n.
+            erewrite Ropp_div. replace (_-_) with (- (pos err)) by lra.
+            apply Ropp_lt_contravar.
+            rewrite Rcomplements.Rlt_div_l.
+            2:{ rewrite S_INR. pose proof pos_INR. apply Rlt_gt.
+                eapply Rle_lt_trans; [eapply H0|].
+                apply Rlt_n_Sn.
+            }
+            rewrite S_INR.
+            rewrite Rmult_comm.
+            rewrite <-Rcomplements.Rlt_div_l.
+            2:{ pose proof (cond_pos err); lra. }
+            rewrite <- Rcomplements.Rlt_minus_l.
+            rewrite -/r. done.
+         ++ pose proof (Rgt_or_ge 0 r).
+            destruct H0.
+            --- exists 0%nat. eapply Rlt_le_trans; last apply pos_INR.
+                lra.
+            --- exists (Z.to_nat (up r)).
+                pose proof (archimed r) as [? ?].
+                rewrite INR_IZR_INZ.
+                rewrite ZifyInst.of_nat_to_nat_eq.
+                rewrite /Z.max.
+                case_match.
+                { rewrite Z.compare_eq_iff in H3.
+                  exfalso. rewrite -H3 in H1. lra.
+                }
+                2: { rewrite Z.compare_gt_iff in H3. exfalso.
+                     assert (IZR (up r) >=0) by lra.
+                     apply IZR_lt in H3. lra. 
+                }
+                lra.
+Qed.             
 
 Lemma exec_ub_epsilon_limit `{ub_clutchGS Σ} (e : expr) σ Z:
    (∀ ε : nonnegreal, ⌜ε > 0⌝ -∗ exec_ub e σ Z ε) -∗ exec_ub e σ Z nnreal_zero.
